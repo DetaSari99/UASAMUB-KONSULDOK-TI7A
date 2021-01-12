1643168bc51f5c517e864e521c604d1347fec1bd
DetaSRY <detasari36@gmail.com>
2021-01-12 15:54:58 +0700

    upload project

---
+++ app/src/main/java/ac/id/atmaluhur/uasamub_konsuldoct_ti7a/HomeAct.java
@@ -0,0 +1,14 @@
+package ac.id.atmaluhur.uasamub_konsuldoct_ti7a;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeAct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
